13d3ba5d612248e550f546594635cd0ad9717136
Evgeniy Poznyak <ek35mm@gmail.com>
2018-02-28 00:27:06 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './App.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+      burger
+      </div>
+    );
+  }
+}
+
+export default App;
