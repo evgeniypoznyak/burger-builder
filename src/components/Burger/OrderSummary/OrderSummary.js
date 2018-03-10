2f2f2f0fd860062cdfae36b623c07b9668beb3a6
Evgeniy Poznyak <evgeniyp@fandiexpress.com>
2018-03-10 01:17:42 -0600

    shouldComponentUpdate checking by props.show

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,6 +5,9 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
 
+    componentWillUpdate() {
+        console.log('OrderSummary componentWillUpdate');
+    }
 
 
     render () {
