93954dccb2bec15b11dcc26b7aa54486549b725a
Evgeniy Poznyak <evgeniyp@fandiexpress.com>
2018-04-12 23:10:50 -0500

    all done

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,6 @@ const sideDrawer = ( props ) => {
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
-
             <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
